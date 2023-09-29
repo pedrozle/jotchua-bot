5c4d7f490f8efe689cbcdb69949993d24816bdbc
Pedrozle <pedrozle@outlook.com>
2023-09-29 18:36:58 -0400

    adicionado comando de tapa

--- src/comandos/social.py
+++ src/comandos/social.py
@@ -62,8 +62,62 @@ class SocialComands(Cog, name="Social"):
 
     @abracar.error
     async def info_error(
-        self, ctx, error
-    ):  # This might need to be (error, ctx), I'm not sure
+        self, ctx: Context, error
+    ):
+        if isinstance(error, commands.BadArgument):
+            await ctx.send(
+                "AHA! :face_with_monocle:\nEsse caba não está no servidor!!!!! :angry:"
+            )
+
+    @commands.command()
+    async def tapa(self, ctx: Context, user: Member = None):
+        """Você dá um tapa no usuário. Chama pra porrada."""
+
+        if not user:
+            response = "Ei!!!! :angry: \nPra quê bater no fantasma :ghost:, cara!? Você precisa informar o usuário.\n\n `ex: tapa fulano`"
+            await ctx.send(response)
+            return
+
+        _guild_id = ctx.guild.id
+        title_header = ""
+        icon_header: any
+        title_content = ""
+        desc_content = ""
+        footer = ""
+
+        for guild in self.client.guilds:
+            if guild.id == _guild_id:
+                title_header = guild.name
+                icon_header = guild.icon
+
+        frases = [
+            "estapeou com vontade",
+            "tirou as luvas e bateu com elas na cara de",
+            "tomou distância e sentou o tapão em",
+        ]
+
+        title_content = "CAI NA PORRADA!"
+
+        desc_content = (
+            f"<@{ctx.message.author.id}> {random.choice(frases)} <@{user.id}>"
+        )
+        footer = f"Porrada em {datetime.now().strftime('%d-%m-%Y')}"
+
+        await ctx.send(
+            embed=embed_msg(
+                ctx,
+                icon_header=icon_header,
+                title_header=title_header,
+                title_content=title_content,
+                desc_content=desc_content,
+                footer=footer,
+            )
+        )
+
+    @tapa.error
+    async def info_error(
+        self, ctx: Context, error
+    ):
         if isinstance(error, commands.BadArgument):
             await ctx.send(
                 "AHA! :face_with_monocle:\nEsse caba não está no servidor!!!!! :angry:"
