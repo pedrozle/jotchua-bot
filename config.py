5a3bd751335ff4d5300414787ead464d924a2212
Pedrozle <pedrozle@outlook.com>
2023-12-08 21:00:03 -0400

    feat [config.py]: Cria arquivo de configuração
    O arquivo de configuração centraliza todos os acessos ao .env e facilita buscar informações

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+class Settings:
+    dotenv_path = Path(".env")
+    load_dotenv(dotenv_path=dotenv_path)
+
+    # Database configurations
+    DB_USER = os.getenv("DB_USER", "")
+    DB_PASS = os.getenv("DB_PASS", "")
+    DB_SERVER = os.getenv("DB_SERVER", "")
+
+    # Bot configurations
+    DEBUG: bool = bool(os.getenv("DEBUG", "False"))
+    BOT_TOKEN: os.getenv("BOT_TOKEN_DEV") if DEBUG else os.getenv("BOT_TOKEN")
+
+def get_settings():
+    return Settings()
