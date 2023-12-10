2f6ec72296c494227682deb4d7a3972b7d28c316
Pedrozle <pedrozle@outlook.com>
2023-12-10 16:16:52 -0400

    refac [userModel.py]: Recriado model User
    Alterado nome da classe, atributos e métodos

---
+++ src/models/user_model.py
@@ -0,0 +1,18 @@
+class UserModel:
+    xp_limit = 100
+
+    def __init__(self, username: str, id: str, level: int = 0, xp: int = 0, money: int = 0):
+        self.username = username
+        self.id = id
+        self.level = level
+        self.xp = xp
+        self.money = money
+        
+    def add_xp(self, points):
+        self.xp += points
+        if self.xp >= self.xp_limit:
+            self.level_up()
+
+    def level_up(self):
+        self.level += 1
+        self.xp_limit =  int(self.xp_limit * 2.5)
